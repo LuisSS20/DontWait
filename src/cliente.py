365811020e6342467ced8480921558b8cfe83587
ismaelmontesinos <montesinosperujoismael@gmail.com>
2021-11-30 13:02:28 +0100

    Cambio los atributos que no corresponden a la lógica de negocio
    He quitado los getters y los setters de los atributos y quitado los atributos fecha de nacimiento y turno actual y añadido el atributo dirección

--- src/cliente.py
+++ src/cliente.py
@@ -4,27 +4,10 @@ class Cliente():
     # Clase cliente
     nombre: str
     correo: str
-    fechaNac: datetime
-    turnoActual: datetime
+    direccion: str
 
 
-    def __init__(self, nombre,correo,fechaNac):
+    def __init__(self, nombre,correo,direccion):
         self.nombre = nombre
         self.correo = correo
-        self.fechaNac = fechaNac
-
-
-    def get_cliente(self):
-        cliente = {"Nombre",self.nombre,
-        "Correo",self.correo,
-        "FechaNac",self.fechaNac}
-        return cliente
-
-    def set_nombre_cliente(self, nombre):
-        self.nombre = nombre
-
-    def set_fechaNac(self,fecha):
-        self.fechaNac = fecha
-
-    def set_correo(self,correo):
-        self.correo = correo
+        self.direccion = direccion
