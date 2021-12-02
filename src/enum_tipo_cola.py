c41483bfe1e20887ac37459b4628bca0d70ecc7d
LuisSS20 <e.luissoriano@go.ugr.es>
2021-12-02 20:58:32 +0100

    Creada enum_tipo_cola.py para tipos de colas

---
+++ src/enum_tipo_cola.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class TiposTurnos(Enum):
+    """
+    Clase enum que representa los tipos de colas permitidos.
+    """
+    PESCADERIA = 1
+    FRUTERIA = 2
+    CARNICERIA = 3
