d66f56260620b17af8f626416dd1a85f34299c8e
LuisSS20 <e.luissoriano@go.ugr.es>
2021-12-09 21:51:13 +0100

    Añado estructura para almacenar turnos atendidos (#16), agrego algoritmo para calcular tiempo de espera medio (#14)

--- src/colas_disponibles.py
+++ src/colas_disponibles.py
@@ -1,5 +1,6 @@
 """Este modulo se encarga de gestionar las colas de la tienda"""
 
+import time
 import enum_tipo_cola as enum
 from src.turno import Turno
 
@@ -27,9 +28,13 @@ class ColasDisponibles:
         if colas_disp is not None:
             for cola in colas_disp:
                 self._check_valid_enum(cola)
+                self.tiempo_medio_colas[cola] = 0
             self.colas_disponibles = colas_disp
         else:
             self.colas_disponibles = {}
+            self.tiempo_medio_colas = {}
+
+        self.dict_turnos_atendidos = {}
 
     def aniadir_nuevo_turno_a_cola(self, n_turno: Turno):
         """
@@ -63,3 +68,54 @@ class ColasDisponibles:
         """
         if enum_cola not in enum.TiposTurnos:
             raise TypeError('El tipo de cola no es válido')
+
+    def comienzo_atender_turno(self, cola: list[Turno]):
+        """
+            Se almacena la hora en la que se ha atendido el turno.
+
+            Parameters
+            ----------
+            cola : list[Turno]
+                Lista de turnos de la cual se coge el primer turno para
+                atenderlo.
+        """
+        cola[0].tiempo_turno = time.time()
+
+    def termino_atender_turno(self, cola: list[Turno]):
+        """
+            Se elimina de la cola el turno que ha sido atendido.
+            Se calcula de nuevo el tiempo medio.
+
+            Parameters
+            ----------
+            cola : list[Turno]
+                Lista de turnos de la cual se coge el primer turno para
+                eliminarlo de la cola.
+        """
+        tiempo_final = time.time()
+        cola[0].tiempo_turno = tiempo_final - cola[0].tiempo_turno
+        # Almaceno el turno que ha sido atentido
+        self.lista_turnos_atendidos.append(cola[0].copy())
+        # Vuelvo a calcular el tiempo medio de espera
+        self.calcular_tiempo_medio_por_turno(
+                    self.dict_turnos_atendidos[cola[0].tipo_cola])
+        # Saco el turno de la cola correspodiente
+        cola.pop()
+
+    def calcular_tiempo_medio_por_turno(self,
+                                        lista_turnos_atendidos: list[Turno]):
+        """
+            Se almacena la hora en la que se ha atendido el turno.
+
+            Parameters
+            ----------
+            cola : list[Turno]
+                Lista de turnos ya atendidos de la cual se extraen los tiempos
+                que han tardado en atenderse cada turno.
+        """
+        sum_tiempo = 0
+        for turno in lista_turnos_atendidos:
+            sum_tiempo += turno.tiempo_turno
+
+        # Agrego el tiempo medio en tiempo_medio_colas[tipo_cola]
+        self.tiempo_medio_colas[lista_turnos_atendidos[0].tipo_cola] = sum_tiempo / len(lista_turnos_atendidos)
