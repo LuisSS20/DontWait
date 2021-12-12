2aaf2162c77edb6ffff01cd98dd500f32bd324ca
LuisSS20 <e.luissoriano@go.ugr.es>
2021-12-12 14:41:38 +0100

    Agrego test_colas_vacias (#18)

---
+++ tests/test_colas.py
@@ -0,0 +1,25 @@
+import pytest
+import sys
+from assertpy import assert_that
+
+sys.path.append('./')
+sys.path.append('./src/')
+from src.colas_disponibles import ColasDisponibles
+from src.turno import Turno
+from src.enum_tipo_cola import TiposTurnos
+
+
+colas_vacias = ColasDisponibles()
+colas_disp = {'Pescaderia': [1, 2, 3, 4, 5, 6],
+              'Carniceria': [],
+              'Fruteria': [1, 2, 3, 4, 5, 6]}
+colas = ColasDisponibles(colas_disp)
+
+
+def test_colas_vacias():
+    assert_that(colas_vacias.colas_disponibles).is_empty()
+
+
+# def test_enum_valido():
+#     for tipo in colas_disp:
+#         assert_that(ColasDisponibles._check_valid_enum(tipo))
