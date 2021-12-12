b4cb73b785df9974b5a2e725e44e6dba3d5defbf
LuisSS20 <e.luissoriano@go.ugr.es>
2021-12-12 19:43:22 +0100

    Muevo metodo check_valid_enum a enum_tipo_cola.py

--- src/enum_tipo_cola.py
+++ src/enum_tipo_cola.py
@@ -10,3 +10,19 @@ class TiposTurnos(Enum):
     Pescaderia = 1
     Fruteria = 2
     Carniceria = 3
+
+    def check_valid_enum(enum_cola):
+        """
+            Validar el tipo de cola que se le esta pasando al enum.
+
+            Parameters
+            ----------
+            enum_cola : Enum
+                Enum que indica a que cola pertenece el turno.
+            Raises
+            ------
+            ValueError
+                Si el enum pasado por no es uno válido.
+        """
+        if not hasattr(TiposTurnos, enum_cola):
+            raise TypeError('El tipo de cola no es válido')
